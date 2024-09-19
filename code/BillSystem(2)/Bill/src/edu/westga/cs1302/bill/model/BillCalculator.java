86dce497a9e8e91106060023bd115cc982bfe3dd
Riley B Ford <rf00032@my.westga.edu>
2024-09-18 22:31:01 -0400

    Implimented getSubtotal
    Added the method getSubtotal that loops through the array and adds the amount of each BillItem to a double that is returned.

--- code/BillSystem(2)/Bill/src/edu/westga/cs1302/bill/model/BillCalculator.java
+++ code/BillSystem(2)/Bill/src/edu/westga/cs1302/bill/model/BillCalculator.java
@@ -2,4 +2,26 @@ package edu.westga.cs1302.bill.model;
 
 public class BillCalculator {
 
+	/** Calculates the subtotal for an array of bill items.
+	 * 
+	 * @precondition items != null
+	 * @postcondition none
+	 * 
+	 * @return subtotal The subtotal of all the items
+	 * 
+	 */
+	public static double getSubtotal(BillItem[] items) {
+		if (items == null) {
+			throw new IllegalArgumentException("Must have Bill Items");
+		}
+		double subtotal = 0;
+		for (int index = 0; index < items.length; index++) {
+			if (items[index] != null) {
+				subtotal = subtotal + items[index].getAmount();
+			} else {
+				break;
+			}
+		}
+		return subtotal;
+	}
 }
